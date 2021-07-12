b5f8c01c7dc58758a3fddcf4b030dac6987db11b
DESKTOP-H79NIM6\Alex <lugovskij@gmail.com>
2021-07-12 12:13:49 +0300

    Добавьте файлы проекта.

---
+++ WindowsAnalistic/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsAnalistic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cursor Position:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "X Y";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Text from Cursor:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(131, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = " ";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(134, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(59, 54);
+            this.panel1.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Color from Cursor:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 189);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Windows Analistic";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsAnalistic/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsAnalistic
+{
+    public partial class Form1 : Form
+    {
+            public struct POINTAPI
+            {
+                public int x;
+                public int y;
+            }
+                  
+            [System.Runtime.InteropServices.DllImport("user32")]
+            public static extern int GetCursorPos(out POINTAPI lpPoint);
+            //public static extern bool GetCursorPos(out POINTAPI lpPoint);
+
+            [System.Runtime.InteropServices.DllImport("user32")]
+            public static extern int GetDC(int hwnd);
+
+            [System.Runtime.InteropServices.DllImport("gdi32")]
+            public static extern int GetPixel(int hdc, int x, int y);
+
+            [System.Runtime.InteropServices.DllImport("user32")]
+            public static extern int ReleaseDC(int hwnd, int hdc);
+
+            [System.Runtime.InteropServices.DllImport("user32")]
+            public static extern int WindowFromPoint(int xPoint, int yPoint);
+
+            [System.Runtime.InteropServices.DllImport("user32")]
+            internal static extern Int32 GetWindowTextLength(IntPtr hwnd);
+
+            [System.Runtime.InteropServices.DllImport("user32.dll")]
+            internal static extern Int32 GetWindowText(Int32 hwnd, System.Text.StringBuilder lpString, Int32 cch);
+
+            [System.Runtime.InteropServices.DllImport("user32")]
+            private static extern int GetWindowText(int hwnd, string lpString, int cch);
+
+        public POINTAPI structCursorPosition;
+        
+        public int w1, x, y, lColor, hdc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if ((timer1.Enabled == true))
+            {
+                timer1.Enabled = false;
+                button1.Text = "Start";
+            }
+            else
+            {
+                timer1.Enabled = true;
+                button1.Text = "Stop";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string GetText(IntPtr hWnd)
+        {
+            int len = GetWindowTextLength(hWnd);
+            if (len > 0)
+            {
+                System.Text.StringBuilder b = new System.Text.StringBuilder('\0', len + 1);
+                var ret = GetWindowText((int)hWnd, b, b.Capacity);
+                if (ret != 0)
+                    return b.ToString();
+                else
+                    return null;
+            }
+            else
+                return null;
+        }
+
+        private string GetCaption(int hWnd)
+        {
+            string capt;
+            int lenC;
+            capt = new String(' ', 255); // заполняем строку пробелами 
+            lenC = GetWindowText(hWnd, capt, 255); // получаем заголовок окна hwnd 
+            capt = capt.Substring(1, lenC); // обрезаем его, оставляя только нужную часть 
+            return capt;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetCursorPos(out structCursorPosition);
+            w1 =  WindowFromPoint(structCursorPosition.x, structCursorPosition.y);
+            hdc = (int)GetDC(0);
+            lColor = GetPixel(hdc, structCursorPosition.x, structCursorPosition.y);
+            ReleaseDC(0, hdc);
+            label2.Text = "X=" + System.Convert.ToString(structCursorPosition.x) + " Y=" + System.Convert.ToString(structCursorPosition.y);
+            label4.Text = GetText((IntPtr)w1);
+            panel1.BackColor = System.Drawing.ColorTranslator.FromOle(lColor);
+
+
+        }
+    }
+}
